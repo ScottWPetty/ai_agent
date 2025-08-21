e0b124f40a711e1249c408971b2459ad0555ec58
ScottWPetty <scottwilsonpetty@gmail.com>
2025-08-20 20:25:03 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+# load the environment variables from the .env file using the dotenv library and read the API key.
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+# Use the API key to create a new instance of a Gemini client.
+client = genai.Client(api_key=api_key)
+
+def main():
+    print("Hello from ai-agent!")
+
+    # Use the client.models.generate_content() method to get a response from the gemini-2.0-flash-001 model (free tier).
+    # The generate_content method returns a GenerateContentResponse object. Print the .txt property of the response
+    # to see the models answer.
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
