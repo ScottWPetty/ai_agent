e5a9b8f75e507e4d826c7b3216d568334a550f70
ScottWPetty <scottwilsonpetty@gmail.com>
2025-08-22 00:03:17 -0500

    added sys.argv prompting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 # load the environment variables from the .env file using the dotenv library and read the API key.
 load_dotenv()
@@ -15,9 +16,19 @@ def main():
     # Use the client.models.generate_content() method to get a response from the gemini-2.0-flash-001 model (free tier).
     # The generate_content method returns a GenerateContentResponse object. Print the .txt property of the response
     # to see the models answer.
+
+    # accept a command line argument for the prompt.
+    # if prompt not provided, print error message and exit with code 1.
+    if len(sys.argv) < 2:
+        print("Error: No argument provided.")
+        sys.exit(1)
+    else:
+        prompt = sys.argv[1]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents=prompt
     )
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
