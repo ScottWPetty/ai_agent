eb3926875dcf2dd091b9d37d7b4be57d89cdb1d2
ScottWPetty <scottwilsonpetty@gmail.com>
2025-08-23 15:41:40 -0500

    added message list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 # load the environment variables from the .env file using the dotenv library and read the API key.
@@ -13,20 +14,33 @@ client = genai.Client(api_key=api_key)
 def main():
     print("Hello from ai-agent!")
 
-    # Use the client.models.generate_content() method to get a response from the gemini-2.0-flash-001 model (free tier).
-    # The generate_content method returns a GenerateContentResponse object. Print the .txt property of the response
-    # to see the models answer.
-
     # accept a command line argument for the prompt.
     # if prompt not provided, print error message and exit with code 1.
+    # Add --verbose CL argument
     if len(sys.argv) < 2:
         print("Error: No argument provided.")
         sys.exit(1)
     else:
         prompt = sys.argv[1]
+        if sys.argv[2] == "--verbose":
+            verbose = True
+        else:
+            verbose = False
 
+    # LLM's work in a conversation. If we keep track of that history, then with each new prompt, the model
+    # can see the entire conversation and respond within the larger context of the conversation.
+    # Each message in the conversation has a "role" (message from either the user or the model). While the program will be "one-shot" for now,
+    # lets update the code to store a list of messages in the conversation, and pass in the "role" appropriately.
+    # Create a new list of types.Content, and set the user's prompt as the only message (for now).
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
+    # Use the client.models.generate_content() method to get a response from the gemini-2.0-flash-001 model (free tier).
+    # The generate_content method returns a GenerateContentResponse object. Print the .txt property of the response
+    # to see the models answer. Update your call to use the messages list.
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
+        model='gemini-2.0-flash-001', contents=messages
     )
 
     print(response.text)
