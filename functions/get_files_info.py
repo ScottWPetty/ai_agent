8e80c73a5a28ab86866dc6488ef66e377a4b6a4b
ScottWPetty <scottwilsonpetty@gmail.com>
2025-08-25 21:08:10 -0500

    worked on get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,15 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    abs_path = os.path.join(working_directory, directory)
+    
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+    
+    joined = os.path.join(working_directory, directory)
+    absolute_path = os.path.abspath(joined)
+    absolute_working_path = os.path.abspath(working_directory)
+
+    if not absolute_path.startswith(absolute_working_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted orking directory'
+    
     
